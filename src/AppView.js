5bea178f8085b33f67f557df91c59cdb5784c5b5
Dakota Lambert <dakotalambertbiz@gmail.com>
2021-09-14 14:50:47 -0400

    MAP SETUP

---
+++ src/AppView.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import { Route } from "react-router-dom";
+import { USMap } from "./components/mapContent/USMap";
+
+export const AppViews = () => {
+	return (
+		<>
+			<USMap />
+		</>
+	);
+};
